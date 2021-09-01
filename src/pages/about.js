be670577686f61ebb6b5b9d4982bc335ccca05a0
Jose <jose.ojeda@sprinklr.com>
2021-08-31 23:48:27 -0700

    added Header to all pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Footer from '../components/footer'
+import Header from '../components/header'
 
 
 const AboutPage = () => {
     return (
         <div>
+            <Header />
             <h1>About Me</h1>
             <p>I currently work as a contractor for Sprinklr</p>
             <p><Link to="/contact">Want to work with me? Reach out</Link> </p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
 import Footer from "../components/footer"
+import Header from "../components/header"
 
 const IndexPage = () => {
   return (
     <div>
+      <Header />
       <h1>Hello,</h1>
       <h2>I'm Jose, a full-stack developer living in Portland</h2>
       <p>Need a developer? <Link to="/contact">Contact me.</Link> </p>
