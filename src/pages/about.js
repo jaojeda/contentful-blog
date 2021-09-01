0b814124092686a0282470f6b45a484a0ceb78eb
Jose <jose.ojeda@sprinklr.com>
2021-09-01 14:46:40 -0700

    updatd pages to use Layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import Footer from '../components/footer'
-import Header from '../components/header'
+import Layout from '../components/layout'
 
 
 const AboutPage = () => {
     return (
-        <div>
-            <Header />
-            <h1>About Me</h1>
-            <p>I currently work as a contractor for Sprinklr</p>
-            <p><Link to="/contact">Want to work with me? Reach out</Link> </p>
-            <Footer />
-        </div>
+      <Layout>
+        <h1>About Me</h1>
+        <p>I currently work as a contractor for Sprinklr</p>
+        <p><Link to="/contact">Want to work with me? Reach out</Link> </p>
+      </Layout>
     )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
-import Footer from "../components/footer"
-import Header from "../components/header"
+import Layout from "../components/layout"
 
 const IndexPage = () => {
   return (
-    <div>
-      <Header />
+    <Layout>
       <h1>Hello,</h1>
       <h2>I'm Jose, a full-stack developer living in Portland</h2>
-      <p>Need a developer? <Link to="/contact">Contact me.</Link> </p>
-      <Footer />
-    </div>
+      <p>Need a developer? <Link to="/contact">Contact me.</Link> </p>  
+    </Layout>
   )
 }
 
