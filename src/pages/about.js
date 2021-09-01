98c866f32a5cad39da667d3e40e414bd148138c3
Jose <jose.ojeda@sprinklr.com>
2021-08-31 22:11:07 -0700

    added links to site

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'gatsby'
+
 
 const AboutPage = () => {
     return (
         <div>
             <h1>About Me</h1>
             <p>I currently work as a contractor for Sprinklr</p>
+            <p><Link to="/contact">Want to work with me? Reach out</Link> </p>
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const IndexPage = () => {
   return (
     <div>
       <h1>Hello,</h1>
       <h2>I'm Jose, a full-stack developer living in Portland</h2>
+      <p>Need a developer? <Link to="/contact">Contact me.</Link> </p>
     </div>
   )
 }
