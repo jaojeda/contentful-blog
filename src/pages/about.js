9ff3dd9f6e37a46541267b660f5a213fad7dfe24
Jose <jose.ojeda@sprinklr.com>
2021-08-31 21:17:16 -0700

    created AboutPage component

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>I currently work as a contractor for Sprinklr</p>
+        </div>
+    )
+}
+
+export default AboutPage
