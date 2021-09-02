471dcc8bab4b78cf3468d7c8b912f3f0b09fc52e
Jose <jose.ojeda@sprinklr.com>
2021-09-01 20:36:11 -0700

    addes balic styling for pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,12 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-sass`],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        implementation: require("node-sass"),
+      },
+    },
+  ]
 }
