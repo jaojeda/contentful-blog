0456e60b1dff59ef846b8319dd934c9bab9759b8
Jose <jose.ojeda@sprinklr.com>
2021-09-01 23:55:11 -0700

    added query to retrieve blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 import Layout from '../components/layout'
 
 const BlogPage = () => {
-    return(
-      <Layout>
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return(
+    <Layout>
         <h1>Blog</h1>
-        <p>Post will show up here later on</p>
+        <ol>
+          {data.allMarkdownRemark.edges.map((edge, i) => {
+            const { title, date} = edge.node.frontmatter
+            return (
+              <li key={`${i-date}`}>
+                <h2>{title}</h2>
+                <p>{date}</p>
+              </li>
+            )
+          })}
+        </ol>
       </Layout>
     )
 }
