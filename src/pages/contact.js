e2ddb6896260053f28c1387590e25fad9cf29282
Jose <jose.ojeda@sprinklr.com>
2021-09-02 15:55:27 -0700

    added helmet to components

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Head from '../components/head';
 import Layout from '../components/layout'
 
 const ContactPage = () => {
     return(
       <Layout>
+        <Head title="Contact" />
         <h1>Contact</h1>
         <p>
             The best way to reach me is <a href="https://twitter.com/JosAOjed">@JosAOjed</a> on Twitter
