916e0920b08c22d37a8eca60abae93f79779bd7e
Jose <jose.ojeda@sprinklr.com>
2021-09-02 17:03:25 -0700

    removed unecessary plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,20 +37,5 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          `gatsby-remark-relative-images`,
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 750,
-              linkImagesToOriginal: false,
-            }
-          }
-        ]
-      }
-    },
   ]
 }
